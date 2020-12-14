de69012bfe423c81d66eb29deacebcb0bd11ebe5
nilu20 <nilu20ishwar@gmail.com>
2020-12-14 16:40:08 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -36,10 +36,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
@@ -64,7 +64,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
+    textSize(30)
+    text(mouseX+","+mouseY,mouseX,mouseY)
 }
 
 function mouseDragged(){
